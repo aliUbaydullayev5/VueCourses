44b442153052fabca5c351a3623c17c8664a7aca
alicoder5 <mukhammadali.ubaydullayev.05@inbox.ru>
2022-08-21 14:05:47 +0500

    Vue Js 3 "Add Count,Vue js Base"

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
+<!-- @format -->
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div>
+		<div>
+			<button v-on:click="addLike">Like</button>
+			<button v-on:click="addDislike">DisLike</button>
+		</div>
+		<p>
+			Кол-во лайков: <strong>{{ likes }}</strong>
+		</p>
+		<p>
+			Кол-во дизлайков: <strong>{{ dislikes }}</strong>
+		</p>
+	</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+	export default {
+		data() {
+			return {
+				likes: 0,
+				dislikes: 0,
+			};
+		},
 
-nav {
-  padding: 30px;
-}
+		methods: {
+			addLike() {
+				this.likes += 1;
+			},
+			addDislike() {
+				this.dislikes += 1;
+			},
+		},
+	};
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
